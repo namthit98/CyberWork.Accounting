f06b4bf2c27c53cc50835ee750988d4a52bafe84
namth <namthit98@gmail.com>
2023-03-29 15:12:15 +0700

    Fix issue

--- src/Application/Common/Interfaces/IIdentityService.cs
+++ src/Application/Common/Interfaces/IIdentityService.cs
@@ -1,8 +1,13 @@
+using CyberWork.Accounting.Application.Common.Models;
+
 namespace CyberWork.Accounting.Application.Common.Interfaces;
 
 public interface IIdentityService
 {
     Task<Guid> CreateUserAsync(string firstName, string lastName,
         string userName, string password, string email);
+    //Task<Guid> UpdateUserAsync(string userId, string firstName, string lastName);
+    //Task<Guid> DeleteUserAsync(string userId);
+    //Task<Guid> ChangePasswordAsync(string userId);
 
 }
